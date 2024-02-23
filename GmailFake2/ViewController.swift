1a6455938845357d4d18ef077dc3942086f5aa35
AnaOlvera123 <personworkadd893@gmail.com>
2024-02-22 19:49:11 -0600

    Initial Commit

---
+++ GmailFake2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GmailFake2
+//
+//  Created by Angel Olvera on 22/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
